0bebf14c7b06b3006d7b556766b516d788afea06
chitraviriyala <92885110+chitraviriyala@users.noreply.github.com>
2021-10-23 17:39:34 -0700

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -4,16 +4,16 @@ import boto3
 # Creating the low level functional client
 client = boto3.client(
     's3',
-    aws_access_key_id = 'AKIA2V3MIZI5ZGRCKQ55',
-    aws_secret_access_key = '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',
+    aws_access_key_id = 'XXXXXXXXXXX',
+    aws_secret_access_key = 'XXXXXXX',
     region_name = 'us-east-1'
 )
     
 # Creating the high level object oriented interface
 resource = boto3.resource(
     's3',
-    aws_access_key_id = 'AKIA2V3MIZI5ZGRCKQ55',
-    aws_secret_access_key = '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',
+    aws_access_key_id = 'XXXXXXXXXX',
+    aws_secret_access_key = 'XXXXXXXXXXXX',
     region_name = 'us-east-1'
 )
 
